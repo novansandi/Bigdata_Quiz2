b1b811af53b0ecbadaeb4428fba4b48811a322a1
novansandi <novansandidwiirawan@gmail.com>
2022-05-19 00:23:55 +0700

    first commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class App 
+{
+    public static void main( String[] args ) throws Exception {
+
+        Job job = Job.getInstance();
+
+        job.setJarByClass(App.class);
+
+        job.setJobName("Statistics");
+
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+
+        job.setMapperClass(RevenueAggregationMapper.class);
+        job.setReducerClass(RevenueAggregationReducer.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(NullWritable.class);
+
+        boolean success = job.waitForCompletion(true);
+        System.exit(success ? 0 : 1);
+    }
+}
